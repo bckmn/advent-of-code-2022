6c04cb9fb43c9957e10fd65623daefa29ed4ef83
Simon Bäckman <simon@bckmn.se>
2022-12-10 22:24:28 +0100

    Refactor solution for day 5 puzzle 1

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,52 +1,56 @@
+use std::array;
+
+type Stacks = [Vec<char>; 9];
+
 fn main() {
     let input = helpers::read_file("inputs", 5);
     let (starting_grid, moves) = parse_input(input);
 
-    println!("Part 1: {:?}", part_one(starting_grid, moves));
-    // println!("Part 2: {}", part_two(&input));
+    println!("Part 1: {}", part_one(starting_grid, moves));
+
+    let input = helpers::read_file("inputs", 5);
+    let (starting_grid, moves) = parse_input(input);
+    println!("Part 2: {}", part_two(starting_grid, moves));
 }
 
-fn part_one(starting_grid: Vec<CrateStack>, moves: Vec<(usize, usize)>) -> Vec<char> {
-    rearrange_crates(starting_grid, moves)
+fn part_one(starting_grid: Stacks, moves: Vec<(usize, usize)>) -> String {
+    let top_crates = rearrange_crates(starting_grid, moves);
+
+    top_crates
+        .iter()
+        .map(|x| x.last().unwrap())
+        .collect()
 }
 
-// fn part_two(input: &str) -> usize {
-//     0
-// }
+fn part_two(starting_grid: Stacks, moves: Vec<(usize, usize)>) -> String {
+    let top_crates = rearrange_crates(starting_grid, moves);
 
-fn parse_input(input: String) -> (Vec<CrateStack>, Vec<(usize, usize)>) {
+    top_crates
+        .iter()
+        .map(|x| x.last().unwrap())
+        .collect()
+}
+
+fn parse_input(input: String) -> (Stacks, Vec<(usize, usize)>) {
     let parts: (&str, &str) = input
         .split_once("\n\n")
         .unwrap();
 
-    (get_starting_grid(parts.0.to_owned()), parse_moves(parts.1.to_owned()))
+    (get_starting_grid(parts.0.to_owned()),
+     parse_moves(parts.1.to_owned()))
 }
 
-fn get_starting_grid(input: String) -> Vec<CrateStack> {
-    let mut rows: Vec<&str> = input
-        .lines()
-        .collect();
-    
-    let column_count = rows.pop().unwrap()
-        .split_whitespace().last().unwrap()
-        .parse::<usize>().unwrap();
-    
-    let mut stacks: Vec<CrateStack> = Vec::new();
-    for _ in 1..column_count+1 {
-        stacks.push(CrateStack { crates: Vec::new() });
-    }
-
-    rows.reverse();
-
-    for row in rows {
-        for (i, ch) in row.chars().enumerate() {
-            if (i == 0 || (i - 1) % 4 == 0) && ch != ' ' && ch !='[' {
-                stacks[i / 4].crates.push(ch);
-            }
-        }
-    }
-    
-    stacks
+fn get_starting_grid(input: String) -> Stacks {
+    array::from_fn(|i| {
+        input
+            .lines()
+            .map(|l| l.chars().skip(1).step_by(4).collect::<Vec<char>>())
+            .rev()
+            .skip(1)
+            .map(|row| row[i])
+            .take_while(|&c| !c.is_whitespace())
+            .collect::<Vec<char>>()
+    })
 }
 
 fn parse_moves(moves: String) -> Vec<(usize, usize)> {
@@ -69,29 +73,14 @@ fn parse_move_line(_line: &str) -> Vec<(usize, usize)> {
     vec![(from - 1, to - 1); count]
 }
 
-fn rearrange_crates(stacks: Vec<CrateStack>, moves: Vec<(usize, usize)>) -> Vec<char> {
+fn rearrange_crates(stacks: Stacks, moves: Vec<(usize, usize)>) -> Stacks {
     let mut stacks = stacks;
     
     for _move in moves {
         let (from, to) = _move;
-        let _crate = stacks[from].pop();
+        let _crate = stacks[from].pop().unwrap();
         stacks[to].push(_crate);
     }
 
-    stacks.into_iter().map(|x| *x.crates.last().unwrap()).collect()
-}
-
-struct CrateStack {
-    crates: Vec<char>,
-}
-
-impl CrateStack {
-
-    fn push(&mut self, _crate: char) {
-        self.crates.push(_crate);
-    }
-
-    fn pop(&mut self) -> char {
-        self.crates.pop().unwrap()
-    }
+    stacks
 }
