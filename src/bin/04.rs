db502ca6ed12503882107c170cb38ddb34d3f9fe
Simon Bäckman <simon@bckmn.se>
2022-12-10 10:26:41 +0100

    solved day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::cmp::{ max, min };
 
 fn main() {
     let input = helpers::read_file("inputs", 4);
@@ -6,43 +7,47 @@ fn main() {
     println!("Part 2: {}", part_two(&input));
 }
 
-pub fn part_one(input: &str) -> u32 {
-    let test = String::from("1-4,3-4");
-    let s = parse_pairs(&test);
-    println!("test: {:?}", s);
-    0
-}
-
-pub fn part_two(input: &str) -> u32 {
-    0
-}
-
-fn parse_pairs(input: &str) -> usize {
+pub fn part_one(input: &str) -> usize {
     input
         .lines()
         .map(|pair| {
-            let a = parse_pair(pair);
-            let elf1: HashSet<&u8> = a[0].iter().collect();
-            let elf2: HashSet<&u8> = a[1].iter().collect();
-            println!("elf1: {:?}, a0: {:?}", elf1, a[0]);
-            println!("elf2: {:?}, a1: {:?}", elf2, a[1]);
-            elf2.is_subset(&elf1)
+            let parsed_pairs = parse_pair(pair);
+
+            let elf1: HashSet<&u8> = parsed_pairs.0.iter().collect();
+            let elf2: HashSet<&u8> = parsed_pairs.1.iter().collect();
+
+            elf2.is_subset(&elf1) || elf1.is_subset(&elf2)
         })
-        .collect::<Vec<bool>>()
-        .into_iter()
-        .filter(|x| *x)
+        .filter(|x| *x == true)
         .count()
 }
 
-fn parse_pair(pair: &str) -> Vec<Vec<u8>> {
+pub fn part_two(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|pair| {
+            let a = parse_pair(pair);
+            
+            max(a.0.first(), a.1.first()) <= 
+            min(a.0.last(), a.1.last())
+        })
+        .count()
+}
+
+fn parse_pair(pair: &str) -> (Vec<u8>, Vec<u8>) {
     pair
-        .split(",")
-        .map(|elf| parse_elf(elf))
-        .collect::<Vec<Vec<u8>>>()
+        .split_once(",")
+        .map(|(elf1, elf2)| (parse_elf(elf1), parse_elf(elf2)))
+        .unwrap()
 }
 
 fn parse_elf(elf: &str) -> Vec<u8> {
-    elf.split("-")
-        .map(|section| section.parse::<u8>().unwrap())
-        .collect::<Vec<u8>>()
+    elf.split_once("-")
+        .map(|(start, end)| {
+            (start.parse::<u8>().unwrap()
+            ..
+            end.parse::<u8>().unwrap() + 1)
+            .collect()
+        })
+        .unwrap()
 }
